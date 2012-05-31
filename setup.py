31f8ca0d682580b9e336c3aca10c92cc569cfccb
Simon Ye <sye737@gmail.com>
2012-05-30 17:25:21 -0700

    Add templates/static to package_data.

--- setup.py
+++ setup.py
@@ -4,14 +4,6 @@ import os
 import djangovoice
 from setuptools import setup, find_packages
 
-data_dirs = [
-    os.path.join('djangovoice', 'static'),
-    os.path.join('djangovoice', 'templates')
-]
-data_files = []
-for data_dir in data_dirs:
-    for dirpath, dirnames, filenames in os.walk(data_dir):
-        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
 
 setup(
     name='django-voice',
@@ -22,7 +14,8 @@ setup(
     url='https://github.com/alageek/django-voice',
     license='BSD',
     packages=find_packages(exclude=('demo', 'demo.*')),
-    data_files = data_files,
+    package_data={'djangovoice': ['static/*',
+                                  'templates/*']},
     zip_safe=False,
     classifiers=[
         "Programming Language :: Python",
