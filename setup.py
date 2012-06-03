d2ebd1a6f51a2f489f6b2f3db4b95391043e134f
Gokmen Gorgen <gokmen@alageek.com>
2012-06-03 15:45:57 +0300

    added platforms, long_description infos.

--- setup.py
+++ setup.py
@@ -2,15 +2,18 @@
 from setuptools import setup, find_packages
 
 djangovoice = __import__('djangovoice')
+description = file('README.rst', 'r')
 
 setup(
     name='django-voice',
     version=djangovoice.get_version(),
     description="A feedback application for Django 1.3 or later",
+    long_description=description.read(),
     author=u'Gökmen Görgen',
     author_email='gokmen@alageek.com',
     url='https://github.com/gkmngrgn/django-voice',
     license='BSD',
+    platforms='any',
     packages=find_packages(exclude=('demo', 'demo.*')),
     include_package_data=True,
     zip_safe=False,
